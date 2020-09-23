1c17a3047ad7be7891c1fb55f5792d464516542b
Yuhui Shen <yuhui.shen@hotmail.com>
2020-09-23 18:55:36 -0400

    Splitted webpack.config.js into webpack.config.dev.js and webpack.config.prod.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: "production",
+    // mode: "production",
     entry: {
         // index: "./lib/index.tsx",
         ui_component_library: "./lib/index.tsx",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
     },
     output: {
         path: path.resolve(__dirname, "dist/lib"),
         library: "ui_component_library",
         libraryTarget: "umd",
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-    },
+
     module: {
         rules: [
             {
@@ -22,24 +23,24 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "ui_component_library",
-            template: "index.html",
-        }),
-    ],
-    externals: {
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react",
-            root: "React",
-        },
-        "react-dom": {
-            commonjs: "react-dom",
-            commonjs2: "react-dom",
-            amd: "react-dom",
-            root: "ReactDOM",
-        },
-    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: "ui_component_library",
+    //         template: "index.html",
+    //     }),
+    // ],
+    // externals: {
+    //     react: {
+    //         commonjs: "react",
+    //         commonjs2: "react",
+    //         amd: "react",
+    //         root: "React",
+    //     },
+    //     "react-dom": {
+    //         commonjs: "react-dom",
+    //         commonjs2: "react-dom",
+    //         amd: "react-dom",
+    //         root: "ReactDOM",
+    //     },
+    // },
 };
