35d8ac62df4093d510a7d9ae4059f2d2df547ce4
Yuhui Shen <yuhui.shen@hotmail.com>
2020-09-27 15:55:37 -0400

    add ci config to have seperate configs for ci and test

--- jest.config.js
+++ jest.config.js
@@ -4,25 +4,21 @@ module.exports = {
     verbose: true,
     clearMocks: false,
 
-    reporters: ["default", "jest-junit"],
+    reporters: ['default'],
 
-    collectCoverage: true,
-    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-    coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov"],
+    collectCoverage: false,
 
-    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
-    moduleDirectories: ["node_modules"],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules'],
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/test/__mocks__/file-mock.js",
-        "\\.(css|less|sass|scss)$":
-            "<rootDir>/test/__mocks__/object-mock.js",
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/test/__mocks__/file-mock.js',
+        '\\.(css|less|sass|scss)$': '<rootDir>/test/__mocks__/object-mock.js',
     },
-    testMatch: ["<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)"],
+    testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
     transform: {
-        "^.+unit\\.(js|jsx)$": "babel-jest",
-        "^.+\\.(ts|tsx)$": "ts-jest",
+        '^.+unit\\.(js|jsx)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'ts-jest',
     },
-    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"],
-};
+    setupFilesAfterEnv: ['<rootDir>test/setupTests.js'],
+}
