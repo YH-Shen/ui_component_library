d459b18aae5e56c13d5ca2ad7c237983086e9e63
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-01 22:16:15 -0400

    add close functions to close and mask

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,65 @@
-import React, { useState } from 'react';
-import Dialog from './dialog';
+import React, { useState } from "react";
+import Dialog from "./dialog";
 
 export default function () {
     const [x, setX] = useState(false);
+    const [y, setY] = useState(false);
+
     return (
         <div>
-            <button onClick={() => setX(!x)}>Click</button>
-            <Dialog visible={x}>
-                <div>hi</div>
-            </Dialog>
+            <div>
+                <h1>Example 1</h1>
+                <button onClick={() => setX(!x)}>Click</button>
+                <Dialog
+                    visible={x}
+                    buttons={[
+                        <button
+                            onClick={() => {
+                                setX(false);
+                            }}
+                        >
+                            OK
+                        </button>,
+                        <button
+                            onClick={() => {
+                                setX(false);
+                            }}
+                        >
+                            Cancel
+                        </button>,
+                    ]}
+                    onClose={() => setX(false)}
+                >
+                    <div>hi</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>Example 2</h1>
+                <button onClick={() => setY(!y)}>Click</button>
+                <Dialog
+                    visible={y}
+                    buttons={[
+                        <button
+                            onClick={() => {
+                                setY(false);
+                            }}
+                        >
+                            OK
+                        </button>,
+                        <button
+                            onClick={() => {
+                                setY(false);
+                            }}
+                        >
+                            Cancel
+                        </button>,
+                    ]}
+                    onClose={() => setY(false)}
+                    onClickMaskClose={true}
+                >
+                    <div>hi</div>
+                </Dialog>
+            </div>
         </div>
     );
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -6,28 +6,45 @@ import { scopedClassMaker } from "../classes";
 import "./dialog.scss";
 interface Props {
     visible: boolean;
+    buttons: Array<React.ReactElement>;
+    onClose: React.MouseEventHandler;
+    onClickMaskClose?: boolean;
 }
 
 const scopedClass = scopedClassMaker("syhui-dialog");
 const sc = scopedClass;
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
+    // type safe
+    const onClickClose: React.MouseEventHandler = (e) => {
+        props.onClose(e);
+    };
+    const onClickMask: React.MouseEventHandler = (e) => {
+        if (props.onClickMaskClose) {
+            props.onClose(e);
+        }
+    };
     return props.visible ? (
         <Fragment>
-            <div className={sc("mask")}></div>
+            <div className={sc("mask")} onClick={onClickMask}></div>
             <div className={sc()}>
-                <div className={sc("close")}>
+                <div className={sc("close")} onClick={onClickClose}>
                     <Icon name="close" />
                 </div>
                 <header className={sc("header")}>Notification</header>
                 <main className={sc("main")}>{props.children}</main>
                 <footer className={sc("footer")}>
-                    <button>OK</button>
-                    <button>Cancel</button>
+                    {props.buttons.map((button, index) =>
+                        React.cloneElement(button, { key: index })
+                    )}
                 </footer>
             </div>
         </Fragment>
     ) : null;
+};
+
+Dialog.defaultProps = {
+    onClickMaskClose: false,
 };
 
 export default Dialog;
