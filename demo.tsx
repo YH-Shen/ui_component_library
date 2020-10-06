f336b81b960b87fb967f7ec0ae3c41e81726a483
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-05 21:33:38 -0400

    improved demo presentation

--- demo.tsx
+++ demo.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
 
 interface Props {
     code: string;
 }
 const Demo: React.FunctionComponent<Props> = (props) => {
+    const [codeVisible, setCodeVisible] = useState(false);
+    const code = (
+        <Highlight {...defaultProps} code={props.code} language="tsx">
+            {({
+                className,
+                style,
+                tokens,
+                getLineProps,
+                getTokenProps,
+            }) => (
+                <pre className={className} style={style}>
+                    {tokens.map((line, i) => (
+                        <div
+                            {...getLineProps({
+                                line,
+                                key: i,
+                            })}
+                        >
+                            {line.map((token, key) => (
+                                <span
+                                    {...getTokenProps({
+                                        token,
+                                        key,
+                                    })}
+                                />
+                            ))}
+                        </div>
+                    ))}
+                </pre>
+            )}
+        </Highlight>
+    );
     return (
         <div>
-            {props.children}
-            <pre>{props.code}</pre>
+            <div className="component-example">{props.children}</div>
+            <div>
+                <button onClick={() => setCodeVisible(!codeVisible)}>
+                    Show Code
+                </button>
+            </div>
+            <pre>
+                {/* Source Code Display */}
+                {codeVisible && code}
+            </pre>
         </div>
     );
 };
