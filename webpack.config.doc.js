50ee59a9b793fbc3053d3741599daaf9cb414455
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-08 21:51:04 -0400

    update

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -14,7 +14,7 @@ module.exports = Object.assign({}, base, {
     plugins: [
         new HtmlWebpackPlugin({
             template: "example.html",
-            filename: "example.html",
+            filename: "index.html",
         }),
     ],
 });
