ba76f0e650ace0cce28e521641e89304dc4a6324
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-08 21:31:36 -0400

    remove lib

--- lib/icon/icon.example.2.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import Icon from "./icon";
-
-const IconExample2: React.FunctionComponent = () => {
-    return <div>Example 2</div>;
-};
-
-export default IconExample2;
