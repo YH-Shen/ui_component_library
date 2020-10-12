268267fba0d73eb5aadd26b9b5cde1fb9cbd9f4c
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-11 23:41:39 -0400

    asych form validation works

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -12,6 +12,7 @@ interface FormRule {
         validate: (value: string) => Promise<void>;
     };
 }
+
 type FormRules = Array<FormRule>;
 
 // interface FormErrors {
@@ -26,54 +27,74 @@ export function noError(errors: any) {
     return Object.keys(errors).length === 0;
 }
 
+interface OneError {
+    message: string;
+    promise?: Promise<any>;
+}
+
 const Validator = (
     formValue: FormValue,
     rules: FormRules,
     callback: (errors: any) => void
 ): void => {
     let errors: any = {};
-    const addError = (key: string, message: string | Promise<any>) => {
+    const addError = (key: string, error: OneError) => {
         if (errors[key] === undefined) {
             errors[key] = [];
         }
-        errors[key].push(message);
+        errors[key].push(error);
     };
     rules.map((rule) => {
         const value = formValue[rule.key];
         if (rule.validator) {
             // Customised Validator
             const promise = rule.validator.validate(value);
-            addError(rule.key, promise);
+            addError(rule.key, { message: "username already exists", promise });
         }
 
         if (rule.required && isEmpty(value)) {
-            addError(rule.key, "Required");
+            addError(rule.key, { message: "Required" });
         }
         if (
             rule.minLength &&
             !isEmpty(value) &&
             value.length < rule.minLength
         ) {
-            addError(rule.key, "Too short. Min length: 6");
+            addError(rule.key, { message: "Too short. Min length: 6" });
         }
         if (
             rule.maxLength &&
             !isEmpty(value) &&
             value.length > rule.maxLength
         ) {
-            addError(rule.key, "Too long. Max length: 16");
+            addError(rule.key, { message: "Too long. Max length: 16" });
         }
         if (rule.pattern && !rule.pattern.test(value)) {
-            addError(rule.key, "Illegal Formate");
+            addError(rule.key, { message: "Illegal Formate" });
         }
         // console.log(rule);
     });
-    Promise.all(flat(Object.values(errors))).then(
+    const promiseList = flat(Object.values(errors))
+        .filter((item) => item.promise)
+        .map((item) => item.promise);
+    Promise.all(promiseList).then(
         () => {
-            callback(errors);
+            const newErrors = fromEntries(
+                Object.keys(errors).map<[string, string[]]>((key) => [
+                    key,
+                    errors[key].map((item: OneError) => item.message),
+                ])
+            );
+            callback(newErrors);
         },
         () => {
-            callback(errors);
+            const newErrors = fromEntries(
+                Object.keys(errors).map<[string, string[]]>((key) => [
+                    key,
+                    errors[key].map((item: OneError) => item.message),
+                ])
+            );
+            callback(newErrors);
         }
     );
     // console.log("errors");
@@ -89,6 +110,14 @@ function flat(array: Array<any>) {
         } else {
             result.push(array[i]);
         }
+    }
+    return result;
+}
+
+function fromEntries(array: Array<[string, string[]]>) {
+    const result: { [key: string]: string[] } = {};
+    for (let i = 0; i < array.length; i++) {
+        result[array[i][0]] = array[i][1];
     }
     return result;
 }
