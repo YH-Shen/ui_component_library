d6b5e337472419b22c66276b488a0b58d50ab6ed
Yuhui Shen <yuhui.shen@hotmail.com>
2020-10-22 22:39:47 -0400

    add layout demo explainations

--- lib/layout/layout.demo.tsx
+++ lib/layout/layout.demo.tsx
@@ -11,7 +11,29 @@ const LayoutDemo = () => {
                 Layout components for scaffolding basic structure of the page:
             </p>
             <p>
-                <code className="little-code">&lt;Layout&gt;:</code>
+                <code className="little-code">&lt;Layout&gt;</code>: wrapper
+                container. When nested with a{" "}
+                <code className="little-code">&lt;Header&gt;</code> or{" "}
+                <code className="little-code">&lt;Footer&gt;</code>, all its
+                child elements will be vertically arranged, except for
+                <code className="little-code">&lt;Aside&gt;</code>.
+            </p>
+            <p>
+                <code className="little-code">&lt;Header&gt;</code>: container
+                for headers.
+            </p>
+            <p>
+                <code className="little-code">&lt;Aside&gt;</code>: container
+                for side sections (usually a side nav). Will be horrizonally
+                placed to any sibling layout component.
+            </p>
+            <p>
+                <code className="little-code">&lt;Content&gt;</code>: container
+                for main sections.
+            </p>
+            <p>
+                <code className="little-code">&lt;Footer&gt;</code>: container
+                for footers.
             </p>
             <h3>Common layouts</h3>
             <Demo code={require("!!raw-loader!./layout.example.1.tsx").default}>
